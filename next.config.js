f7e58862b4a875a23cbb9ab3c8dbcd9afe599bd4
GurshaanS05 <shaanusingh41@gmail.com>
2025-02-11 16:12:55 -0500

    updated more files

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
- 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    basePath: process.env.NODE_ENV === 'production' ? '/<iloverhea>' : '',
+    images: {
+      unoptimized: true,
+    },
+  }
+  
+  module.exports = nextConfig
